1d6c831788754dde2be15325f41801df52581616
ZHANG JIANING <jianing.zhang@channing.harvard.edu>
2024-02-09 09:39:31 -0500

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,12 @@
+## Please visit the online version in https://bmpmaster.streamlit.app
+
 import streamlit as st
 import pandas as pd
 import os
 
 st.write("""
-# My first app
-Hello *world!*
+# Biological and Medical Paper Master (BMPM)
+*BMPM* is a LLM-driven reference paper reader. It applies the retrieval-augument generation framework to study the custom-defined paper library. 
 """)
 
 datDir = "data"
